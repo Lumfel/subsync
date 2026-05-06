04f5e9d323b106a05c65c95cf5b28028da73607f
Lumfel <paulfelippegtibooc@gmail.com>
2026-05-07 07:32:01 +0800

    paul

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,13 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
-    protected $fillable = [
-        'house_id',
-        'status_type'
-    ];
-
-    public function household()
-    {
-        return $this->belongsTo(Household::class, 'house_id');
-    }
+    //
 }
